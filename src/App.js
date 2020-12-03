319155fd03667d52482cda596f737be4c1e1cf39
mattenbar <matthw.enbar@gmail.com>
2020-12-03 11:06:43 -0500

    capabilities and blue mountain bottom img added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import AboutUs from './components/aboutUs.js'
+import Capabilities from './components/capabilities'
+import BlueMountainBottomImg from './images/BlueMountainBottomImg.jpg'
+
 
 function App() {
   return (
     <div className="App">
       <AboutUs />
+      <Capabilities />
+      <img width= "100%" src={BlueMountainBottomImg} alt="BlueMountainImg"/>
     </div>
   );
 }
---
+++ src/components/capabilities.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+
+class Capabilities extends Component {
+
+  render(){
+
+    let capabilitiesList = (
+      <ul>
+        <li>Venture capital investments for early-stage wealth management stat-ups</li>
+        <li>Business incubator solutions for fin-tech companies </li>
+        <li>Growth and customer acquisition strategy </li>
+        <li>Strategic planning and execution support </li>
+        <li>M&A advisory and private capital raising transactions </li>
+        <li>Board advisory services</li>
+      </ul>
+    )
+
+    return(
+      <div class="capabilities" id="capabilities">
+    <h4>CAPABILITIES</h4>
+    
+    <p>
+      We create significant and long-term value for wealth management businesses by bringing
+      <br/>
+      together the right capital structure with the right strategic plan.
+    </p>
+    <p>
+      Through our seasoned internal team and in collaboration with long standing strategic partners, 
+      <br/>
+      our capabilities encompass:
+      ${capabilitiesList}
+      <br/>
+    </p>
+
+  </div>
+    )
+  }
+
+}
+
+export default Capabilities
