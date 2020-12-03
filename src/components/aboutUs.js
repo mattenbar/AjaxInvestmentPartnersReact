5f84ba3e2730457bee3e9831c5a4b4cc2f8d02ba
mattenbar <matthw.enbar@gmail.com>
2020-12-03 12:40:11 -0500

    about us styling complete

--- src/components/aboutUs.js
+++ src/components/aboutUs.js
@@ -7,24 +7,24 @@ class AboutUs extends Component{
       <div class="about" id="about">
     <br/>
     <br/>
-    <h4>ABOUT US</h4>
+    <h3>ABOUT US</h3>
     <p>
-      Ajax Investment Partners aligns <b>financial and intellectual capital, strategic relationships and transformational growth
-      strategies</b> with financial services businesses that have a unique vision for creative disruption that drives platform,
+      Ajax Investment Partners aligns <span className="blue">financial and intellectual capital, strategic relationships and transformational growth
+      strategies</span> with financial services businesses that have a unique vision for creative disruption that drives platform,
       value, and scale.
     </p>
     <span class="blueLine"></span>
     <p>
-      We are an <b>incubator</b> of early-stage companies, a <b>growth accelerator</b> for firms hitting their stride and a <b>strategic
-      partner</b> for mature  enterprises seeking fresh investment  and new initiatives to  fuel  growth.
+      We are an <span>incubator</span> of early-stage companies, a <span>growth accelerator</span> for firms hitting their stride and a <span>strategic
+      partner</span> for mature  enterprises seeking fresh investment  and new initiatives to  fuel  growth.
     </p>
     <span class="blueLine"></span>
     <p>
       From providing seed capital for new ventures, to facilitating growth equity investment transactions, to identify-
       <br/>
-      ing new avenues for profitable expansion, Ajax Investment Partners combines a passion for the <b>transformative
+      ing new avenues for profitable expansion, Ajax Investment Partners combines a passion for the <span>transformative
       <br/>
-      power of financial services</b>, with a <b>commitment to driving value</b> for our portfolio companies, our strategic 
+      power of financial services</span>, with a <span>commitment to driving value</span> for our portfolio companies, our strategic 
       <br/>
       partners and our clients.
     </p>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
-import Logo from '../images/AJAXLOGO.svg'
+import Logo from '../AJAXLOGO.svg'
 
 
 class NavBar extends React.Component{
