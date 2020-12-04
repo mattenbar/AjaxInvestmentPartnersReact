bcc8908ec5267e4467c6c203c42552d646714a7c
mattenbar <matthw.enbar@gmail.com>
2020-12-04 14:15:11 -0500

    formatting

--- src/components/header.js
+++ src/components/header.js
@@ -17,27 +17,27 @@ class Header extends React.Component{
       <Navbar className="nav">
         <Navbar.Brand href="#home"><img
             src={Logo}
-            width="25%"
+            width="45%"
             className="d-inline-block align-top"
             alt="AJAX LOGO"
           /></Navbar.Brand>
         <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
+        <Navbar.Collapse className="justify-content-end" >
           <Navbar.Text style={{display: "flex"}}>
-            <a href="#about">ABOUT&nbsp;US&nbsp;&nbsp;</a>
-            <a href="#capabilities">&nbsp;CAPABILITIES&nbsp;&nbsp;</a>
-            <a href="#leadership">&nbsp;LEADERSHIP&nbsp;&nbsp;</a>
-            <a href="#footer"><button className="contact-button" href="#footer">CONTACT&nbsp;US</button></a>
+            <a href="#about">ABOUT&nbsp;US&nbsp;&nbsp;&nbsp;</a>
+            <a href="#capabilities">&nbsp;CAPABILITIES&nbsp;&nbsp;&nbsp;</a>
+            <a href="#leadership">&nbsp;LEADERSHIP&nbsp;&nbsp;&nbsp;</a>
+            <a href="#footer"><button className="contact-button" href="#footer">CONTACT&nbsp;US</button></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           </Navbar.Text>
         </Navbar.Collapse>
       </Navbar>
       </Row>
       <Row className="alignCenter" style={{textAlign: "center"}}> 
-        <h3  >
+        <h3>
           ALIGNING&nbsp;&nbsp;FINANCIAL&nbsp;&nbsp;AND&nbsp;&nbsp;INTELLECTUAL&nbsp;&nbsp;CAPITAL,&nbsp;&nbsp;STRATEGIC 
-          <br/>
+          <br /><br />
           RELATIONSHIPS&nbsp;&nbsp;AND&nbsp;&nbsp;TRANSFORMATIONAL&nbsp;&nbsp;GROWTH&nbsp;&nbsp;STRATEGIES
-          <br />
+          <br /><br />
           FOR&nbsp;&nbsp;FINANCIAL&nbsp;&nbsp;SERVICES&nbsp;&nbsp;BUSINESSES
         </h3> 
       </Row>
